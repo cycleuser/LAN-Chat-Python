6e85fa6090f0fed2bc10966a3174f31e1239a745
DashKiwi <Maxmooney08@gmail.com>
2024-09-26 23:26:17 +1200

    Added Basic functionality for localhost
    Added the ability to connect and chat with your own computer.
    
    Fix the the text overlapping each other when it recieves it from another device and causing a blank line
    
    Make it possible to join from device other than localhost

---
+++ client.py
@@ -0,0 +1,61 @@
+import socket
+import errno
+import sys
+import threading
+
+HEADER_LENGTH = 10
+while True:
+    try:
+        IP = input("Please enter a IP: ")
+        PORT = int(input("Please enter a PORT: "))
+        my_username = input("Username: ")
+
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        client_socket.connect((IP, PORT))
+        client_socket.setblocking(False)
+        print(f"Connected to server on {IP}:{PORT}")
+        break
+    except:
+        print("Please try again. Server not found!")
+
+username = my_username.encode('utf-8')
+username_header = f"{len(username):<{HEADER_LENGTH}}".encode('utf-8')
+
+client_socket.send(username_header + username)
+
+def send_msg_to_server():
+    while True:
+        message = input(f"{my_username} > ")
+        if message:
+            message = message.encode('utf-8')
+            message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
+            client_socket.send(message_header + message)
+
+threading.Thread(target=send_msg_to_server).start()
+
+while True:
+    try:
+        while True:
+            username_header = client_socket.recv(HEADER_LENGTH)
+            if not len(username_header):
+                print("Connection closed by the server")
+                sys.exit()
+            username_length = int(username_header.decode('utf-8').strip())
+            username = client_socket.recv(username_length).decode('utf-8')
+            message_header = client_socket.recv(HEADER_LENGTH)
+            message_length = int(message_header.decode('utf-8').strip())
+            message = client_socket.recv(message_length).decode('utf-8')
+            #sys.stdout.write('\x1b[1A') 
+            print('\033[G\033[2K', end="", flush=True)
+            print(f"{username} > {message}")
+
+    except IOError as e:
+        if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
+            print('Reading error: {}'.format(str(e)))
+            sys.exit()
+        continue
+
+    except Exception as e:
+        print('Reading error: '.format(str(e)))
+        sys.exit()
